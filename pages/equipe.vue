6a6ed4e6da3e3fa4ba0ffe418a75b913a2af77ff
FionaBourgaux <fiona.ifosup@gmail.com>
2024-01-02 17:38:06 +0100

    espace

--- pages/equipe.vue
+++ pages/equipe.vue
@@ -17,167 +17,226 @@
     de la réalité virtuelle pour le bien-être de vos animaux de compagnie.
     Préparez-vous à plonger dans le monde captivant de Paws & Play VR, où la
     passion et l'expertise se rencontrent pour créer une expérience hors du
-    commun.</TextesParagraphe>
+    commun.</TextesParagraphe
+  >
 
-   <TitresH2 class="text-center mt-10 "> Équipe de Direction : </TitresH2>
+  
+    <TitresH2 class="text-center mt-10"> Équipe de Direction : </TitresH2>
 
- <div class="grid grid-cols-3 m-8 ">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-5.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Alex Mitchell
+    <div class="grid grid-cols-3 mt-10 pl-20">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-5.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Alex Mitchell
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            Directeur des Opérations
+          </div>
+
+          <p class="text-gray-700 text-base">
+            En tant que Directeur des Opérations, Alex Mitchell assure que
+            chaque aspect de Paws & Play VR fonctionne en harmonie. Son
+            engagement envers l'excellence opérationnelle garantit une
+            expérience optimale pour nos utilisateurs.
+          </p>
         </div>
+      </div>
 
-        <div class="font-light text-xl mb-2 text-black text-center">
-         Directeur des Opérations
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-6.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Eva Thompson
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            PDG et Co-fondatrice
+          </div>
+          <p class="text-gray-700 text-base">
+            À la tête de Paws & Play VR, Eva Thompson combine sa passion pour la
+            technologie et son amour des animaux pour créer des expériences
+            uniques. Son leadership visionnaire guide l'équipe vers
+            l'avant-garde de l'innovation.
+          </p>
         </div>
+      </div>
 
-        <p class="text-gray-700 text-base">
-         En tant que Directeur des Opérations, Alex Mitchell assure que chaque aspect de Paws & Play VR fonctionne en harmonie. Son engagement envers l'excellence opérationnelle garantit une expérience optimale pour nos utilisateurs.
-        </p>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-7.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Danièle Rodriguez
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            Directrice Technique
+          </div>
+          <p class="text-gray-700 text-base">
+            Danièle Rodriguez, notre Directrice Technique, est le cerveau
+            derrière l'innovation technologique de Paws & Play VR. Sa passion
+            pour la réalité virtuelle façonne l'avenir des expériences
+            interactives pour les animaux.
+          </p>
+        </div>
       </div>
     </div>
 
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-6.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Eva Thompson
-        </div>
+    <TitresH2 class="text-center mt-10">
+      Marketing et Communication :
+    </TitresH2>
 
-        <div class="font-light text-xl mb-2 text-black text-center">
-          PDG et Co-fondatrice
+    <div class="grid grid-cols-3 m-8 pl-20">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-1.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Isabella Taylor
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            Responsable Marketing
+          </div>
+
+          <p class="text-gray-700 text-base">
+            Isabella Taylor allie une expertise marketing exceptionnelle à une
+            passion pour les animaux. Sa créativité stimule la croissance de
+            notre communauté.
+          </p>
         </div>
-        <p class="text-gray-700 text-base">
-        À la tête de Paws & Play VR, Eva Thompson combine sa passion pour la technologie et son amour des animaux pour créer des expériences uniques. Son leadership visionnaire guide l'équipe vers l'avant-garde de l'innovation.
-        </p>
+      </div>
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-2.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Nathan Clarcks
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            Responsable des Médias Sociaux
+          </div>
+          <p class="text-gray-700 text-base">
+            En charge des médias sociaux, Nathan Clark partage les histoires
+            captivantes de nos utilisateurs. Sa connexion avec la communauté
+            renforce nos liens.
+          </p>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-8.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Sophie Ramirez
+          </div>
+
+          <div class="font-light text-xl mb-2 text-black text-center">
+            Responsable Marketing Créatif :
+          </div>
+
+          <p class="text-gray-700 text-base">
+            Sophie Ramirez apporte une approche novatrice au marketing de Paws &
+            Play VR. Sa créativité sans bornes et son expertise en communication
+            visuelle sont à l'origine de campagnes qui captivent les amateurs
+            d'animaux et les passionnés de technologie.
+          </p>
+        </div>
+      </div>
+    </div>
+    <TitresH2 class="text-center mt-10">
+      Développement et Conception :
+    </TitresH2>
+
+    <div class="grid grid-cols-3 m-8 pl-20">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-3.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Sophie Anderson
+          </div>
+          <div class="font-light mb-2 text-black text-center text-xl">
+            Chef Développeur VR
+          </div>
+          <p class="text-gray-700 text-base">
+            En tant que Chef Développeur VR, Sophie Anderson transforme des
+            idées créatives en expériences virtuelles exceptionnelles. Son
+            expertise technique donne vie à l'imagination.
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/persona-9.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Oliver Parker
+          </div>
+          <div class="font-light mb-2 text-black text-center text-xl">
+            Illustrateur Principal
+          </div>
+          <p class="text-gray-700 text-base">
+            Oliver Parker, notre talentueux illustrateur principal, donne vie à
+            des mondes virtuels colorés qui enchantent les utilisateurs et
+            captivent leurs animaux de compagnie. Sa créativité débordante et
+            son œil artistique aiguisé font de chaque expérience une aventure
+            visuelle mémorable.
+          </p>
+        </div>
+      </div>
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <NuxtImg
+          class="h- w- center"
+          src="/images/person-4.jpeg"
+          alt="équipe"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-black text-center">
+            Maxwell Lewis
+          </div>
+          <div class="font-light mb-2 text-black text-center text-xl">
+            Designer Principal
+          </div>
+          <p class="text-gray-700 text-base">
+            En charge des médias sociaux, Nathan Clark partage les histoires
+            captivantes de nos utilisateurs. Sa connexion avec la communauté
+            renforce nos liens.
+          </p>
+        </div>
       </div>
     </div>
 
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-7.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Danièle Rodriguez
-        </div>
-
-        <div class="font-light text-xl mb-2 text-black text-center">
-          Directrice Technique
-        </div>
-        <p class="text-gray-700 text-base">
-        Danièle Rodriguez, notre Directrice Technique, est le cerveau derrière l'innovation technologique de Paws & Play VR. Sa passion pour la réalité virtuelle façonne l'avenir des expériences interactives pour les animaux.
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <TitresH2 class=" text-center mt-10"> Marketing et Communication : </TitresH2>
-
-  <div class="grid grid-cols-3 m-8 ">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-1.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Isabella Taylor
-        </div>
-
-        <div class="font-light text-xl mb-2 text-black text-center">
-          Responsable Marketing
-        </div>
-
-        <p class="text-gray-700 text-base">
-          Isabella Taylor allie une expertise marketing exceptionnelle à une
-          passion pour les animaux. Sa créativité stimule la croissance de notre
-          communauté.
-        </p>
-      </div>
-    </div>
-
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-2.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Nathan Clarcks
-        </div>
-
-        <div class="font-light text-xl mb-2 text-black text-center">
-          Responsable des Médias Sociaux
-        </div>
-        <p class="text-gray-700 text-base">
-          En charge des médias sociaux, Nathan Clark partage les histoires
-          captivantes de nos utilisateurs. Sa connexion avec la communauté
-          renforce nos liens.
-        </p>
-      </div>
-    </div>
-     <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-8.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Sophie Ramirez
-        </div>
-
-        <div class="font-light text-xl mb-2 text-black text-center">
-           Responsable Marketing Créatif :
-        </div>
-
-        <p class="text-gray-700 text-base">
-        Sophie Ramirez apporte une approche novatrice au marketing de Paws & Play VR. Sa créativité sans bornes et son expertise en communication visuelle sont à l'origine de campagnes qui captivent les amateurs d'animaux et les passionnés de technologie.
-        </p>
-      </div>
-    </div>
-  </div>
-  <TitresH2 class=" text-center mt-10"> Développement et Conception : </TitresH2>
-
-  <div class="grid grid-cols-3 m-8">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-3.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Sophie Anderson
-        </div>
-        <div class="font-light mb-2 text-black text-center text-xl">
-          Chef Développeur VR
-        </div>
-        <p class="text-gray-700 text-base">
-          En tant que Chef Développeur VR, Sophie Anderson transforme des idées
-          créatives en expériences virtuelles exceptionnelles. Son expertise
-          technique donne vie à l'imagination.
-        </p>
-      </div>
-    </div>
-
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/persona-9.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Oliver Parker
-        </div>
-        <div class="font-light mb-2 text-black text-center text-xl">
-          Illustrateur Principal
-        </div>
-        <p class="text-gray-700 text-base">
-          Oliver Parker, notre talentueux illustrateur principal, donne vie à des mondes virtuels colorés qui enchantent les utilisateurs et captivent leurs animaux de compagnie. Sa créativité débordante et son œil artistique aiguisé font de chaque expérience une aventure visuelle mémorable.
-        </p>
-      </div>
-    </div>
-
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <NuxtImg class="h- w- center" src="/images/person-4.jpeg" alt="équipe" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-black text-center">
-          Maxwell Lewis
-        </div>
-        <div class="font-light mb-2 text-black text-center text-xl">
-          Designer Principal
-        </div>
-        <p class="text-gray-700 text-base">
-          En charge des médias sociaux, Nathan Clark partage les histoires
-          captivantes de nos utilisateurs. Sa connexion avec la communauté
-          renforce nos liens.
-        </p>
-      </div>
-    </div>
-  </div>
 </template>
